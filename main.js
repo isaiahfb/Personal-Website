be43cef508a317bd305ff71afbcb07a45096e975
isaiahfb <isaiah.fujii@gmail.com>
2020-08-19 14:29:51 -0700

    cleaned up comments

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 
 
-function showSkills(){
-    document.getElementById("skills").innerHTML = "Python, Java, C++, JavaScript, HTML/CSS";
-}
-
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function addResponsive() {
     var x = document.getElementById("myTopnav");
@@ -15,18 +11,15 @@ function addResponsive() {
   }
 
 
-// When the user scrolls the page, execute myFunction
+
+
+// make the nav bar sticky
 window.onscroll = function() {stickNavBar()};
 
-
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickNavBar() {
 
-    // Get the navbar
     var navbar = document.getElementById("myTopnav");
 
-    // Get the offset position of the navbar
     var sticky = navbar.offsetTop;
     
     if (window.pageYOffset >= sticky) {
